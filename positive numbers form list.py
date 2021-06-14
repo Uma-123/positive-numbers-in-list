2f8b98f4c22a9a4d85516072227f027312981331
Uma-123 <64482381+Uma-123@users.noreply.github.com>
2021-06-14 17:42:33 +0530

    Create positive numbers form list.py
    This program asks the user to input how many numbers he wants to enter into the list then it prints only the positive numbers in the list in the form of list

---
+++ positive numbers form list.py
@@ -0,0 +1,15 @@
+a = int(input("Enter number of terms you want to enter: "))
+c = []
+for i in range(a):
+    b = input()
+    c.append(b)
+d = []
+for j in c:
+    x = int(j)
+    if x>0:
+        d.append(x)
+    
+print("Input: ",c)    
+print("Output: ",d)
+
+
